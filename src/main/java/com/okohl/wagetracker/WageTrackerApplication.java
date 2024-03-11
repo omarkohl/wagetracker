60f3eb9a25e11fd1e19d2b3cbc4ee603fb98eb09
Omar Kohl <omarkohl@posteo.net>
2024-03-11 10:08:08 +0100

    build: initialize Spring Boot application with Spring Initializr

---
+++ src/main/java/com/okohl/wagetracker/WageTrackerApplication.java
@@ -0,0 +1,13 @@
+package com.okohl.wagetracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WageTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WageTrackerApplication.class, args);
+	}
+
+}
